34cc47f5d833477337a9d314d3d700651ef9e8ef
hiagoCf19 <hiagoferreira@id.uff.br>
2024-06-03 13:07:48 -0300

    feat: add search

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -25,10 +25,12 @@ import RegisterProduct from "@/components/register-product-dialog";
 export default function Home() {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<DataApi>()
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     getData("products", setData, setLoading);
   }, []);
+  const filterProducts = search !== '' ? data?.products.filter(p => p.name.toLowerCase().includes(search.toLowerCase())) : data?.products
 
   return (
     <div className=" sm:pl-44">
@@ -36,49 +38,57 @@ export default function Home() {
         <div className="flex justify-between pt-4">
           <h1 className="font-semibold text-xl">Produtos:</h1>
           <div className="flex items-center gap-2">
-            <Input type="search" className="focus-visible:ring-1 p-4" placeholder="Buscar produto" />
+            <Input type="search" className="focus-visible:ring-1 p-4" placeholder="Buscar produto" value={search} onChange={(e) => setSearch(e.target.value)} />
             <Button variant={"destructive"} className="p-2">
               <Search size={22} />
             </Button>
           </div>
         </div>
-        <Table className="[&::-webkit-scrollbar]:hidden">
-          <TableHeader className="">
-            <TableRow className="text-xs sm:text-sm  ">
-              <TableHead className="text-zinc-50">Código</TableHead>
-              <TableHead className="text-zinc-50">Nome</TableHead>
-              <TableHead className="text-zinc-50">Preço final</TableHead>
-              <TableHead className="text-right sm:text-start text-zinc-50">Estoque</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody className="[&::-webkit-scrollbar]:hidden">
-            {data?.products.map((product) => (
-              <TableRow key={product.id} className="text-sm">
-                <TableCell className="font-medium pl-6">{product.id}</TableCell>
-                <TableCell className="text-muted-foreground max-w-[170px] px-0">{product.name}</TableCell>
-                <TableCell className="text-muted-foreground">{formatCurrency(calculateProducTotalPrice(product))}</TableCell>
-                <TableCell className="text-right sm:text-start text-muted-foreground">
-                  {product.quantity_in_stock} Unidades
-                </TableCell>
-                <TableCell className=" text-right">
-                  <AlertDialog>
-                    <AlertDialogTrigger>
-                      <Button size='icon' className="size-9 p-2" variant={'outline'} asChild >
-                        <Ellipsis />
-                      </Button>
-                    </AlertDialogTrigger>
-                    <ProductCard
-                      loading={loading}
-                      product={product}
-                      setData={setData}
-                      setIsLoading={setLoading}
-                    />
-                  </AlertDialog>
-                </TableCell>
+        <div className=" [&::-webkit-scrollbar]:hidden overflow-y-scroll max-h-[760px]">
+          <Table >
+            <TableHeader >
+              <TableRow className="text-xs sm:text-sm">
+                <TableHead className="text-zinc-50">Código</TableHead>
+                <TableHead className="text-zinc-50">Nome</TableHead>
+                <TableHead className="text-zinc-50">Preço final</TableHead>
+                <TableHead className="text-right sm:text-start text-zinc-50">Estoque</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table >
+            </TableHeader>
+            <TableBody>
+              {filterProducts?.map((product) => (
+                <TableRow key={product.id} className="text-sm">
+                  <TableCell className="font-medium pl-6">{product.id}</TableCell>
+                  <TableCell className="text-muted-foreground max-w-[170px] px-0">{product.name}</TableCell>
+                  <TableCell className="text-muted-foreground">{formatCurrency(calculateProducTotalPrice(product))}</TableCell>
+                  <TableCell className="text-right sm:text-start text-muted-foreground">
+                    {product.quantity_in_stock} Unidades
+                  </TableCell>
+                  <TableCell className=" text-right">
+                    <AlertDialog>
+                      <AlertDialogTrigger>
+                        <Button size='icon' className="size-9 p-2" variant={'outline'} asChild >
+                          <Ellipsis />
+                        </Button>
+                      </AlertDialogTrigger>
+                      <ProductCard
+                        loading={loading}
+                        product={product}
+                        setData={setData}
+                        setIsLoading={setLoading}
+                      />
+                    </AlertDialog>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table >
+        </div>
+        {filterProducts?.length === 0 ?
+          <div className="flex justify-center flex-col text-center py-5">
+            <p className="text-2xl font-medium">Nenhum resultado encontrado para "{search}".</p>
+            <span className="text-muted-foreground text-center text-sm"> Verifique se você digitou corretamente e tente novamente!</span>
+          </div>
+          : null}
         <div className="flex justify-center py-5">
           {loading && (<Loader2 className="animate-spin text-muted-foreground" />)}
         </div>
